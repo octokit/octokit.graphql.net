31d6a10d2182d5becf463ee289b6a28885a65b0c
Steven Kirk <grokys@gmail.com>
2016-11-18 12:46:37 +0100

    Added a Octiquit deserialization test.
    And made it pass.

--- LinqToGraphQL/Deserializers/ResponseDeserializer.cs
+++ LinqToGraphQL/Deserializers/ResponseDeserializer.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using LinqToGraphQL.Syntax;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace LinqToGraphQL.Deserializers
@@ -29,19 +31,71 @@ namespace LinqToGraphQL.Deserializers
                 {
                     var resultField = data[field.Name];
 
-                    if (field.ResultType != null)
-                    {
-                        yield return resultField.ToObject(field.ResultType);
-                    }
-                    else if (field.ResultConstructor != null)
+                    if (field.ResultConstructor != null)
                     {
                         var args = DeserializeSelectionSet(field, resultField).ToArray();
+                        PrepareArgs(field.ResultConstructor.GetParameters(), args);
                         yield return field.ResultConstructor.Invoke(args);
+                    }
+                    else if (field.ResultType != null)
+                    {
+                        var queryableType = GetQueryableType(field.ResultType);
+                        var resultArray = resultField as JArray;
+
+                        if (queryableType == null)
+                        {
+                            yield return resultField.ToObject(field.ResultType);
+                        }
+                        else if (queryableType != null && resultArray != null)
+                        {
+                            var itemType = field.ResultType.GetGenericArguments()[0];
+                            var arrayType = itemType.MakeArrayType();
+                            yield return resultField.ToObject(arrayType);
+                        }
+                        else
+                        {
+                            var result = resultField.ToObject(queryableType);
+                            yield return ConstructQueryable(queryableType, result);
+                        }
                     }
                     else
                     {
                         yield return DeserializeSelectionSet(field, resultField).Single();
                     }
+                }
+            }
+        }
+
+        private static Type GetQueryableType(Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IQueryable<>) ?
+                type.GetGenericArguments()[0] : null;
+        }
+
+        private object ConstructQueryable(Type queryableType, object item)
+        {
+            var enumerableType = typeof(IEnumerable<>).MakeGenericType(queryableType);
+            var asQueryable = typeof(Queryable).GetMethod(nameof(Queryable.AsQueryable), new[] { enumerableType });
+            var array = Array.CreateInstance(queryableType, 1);
+            array.SetValue(item, 0);
+            return asQueryable.Invoke(null, new[] { array });
+        }
+
+        private void PrepareArgs(ParameterInfo[] parameters, object[] args)
+        {
+            if (parameters.Length != args.Length)
+            {
+                throw new Exception("Wrong number of parameters.");
+            }
+
+            for (var i = 0; i < parameters.Length; ++i)
+            {
+                var p = parameters[i];
+                var queryableType = GetQueryableType(p.ParameterType);
+
+                if (queryableType == args[i].GetType())
+                {
+                    args[i] = ConstructQueryable(queryableType, args[i]);
                 }
             }
         }
---
+++ Octoqit.UnitTests/ResponseDeserializerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using LinqToGraphQL.Builders;
+using LinqToGraphQL.Deserializers;
+using Xunit;
+
+namespace Octoqit.UnitTests
+{
+    public class ResponseDeserializerTests
+    {
+        [Fact]
+        public void RepositoryOwner_Repository_Query()
+        {
+            var query = new RootQuery()
+                .RepositoryOwner("grokys")
+                .Repository("VisualStudio")
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                    Owner = x.Owner.Select(o => new
+                    {
+                        o.Login
+                    }),
+                    x.IsFork,
+                    x.IsPrivate,
+                });
+            string data = @"{
+  ""data"": {
+    ""repositoryOwner"": {
+      ""repository"": {
+        ""id"": ""1234"",
+        ""name"": ""LinqToGraphQL"",
+        ""owner"": {
+          ""login"": ""grokys""
+        },
+        ""isFork"": false,
+        ""isPrivate"": false
+      }
+    }
+  }
+}";
+
+            var operation = new QueryBuilder().Build(query.Expression);
+            var expectedType = query.GetType().GetGenericArguments()[0];
+            dynamic result = new ResponseDeserializer().Deserialize(operation, data);
+
+            Assert.IsType(expectedType, result);
+            Assert.Equal("1234", result.Id);
+            Assert.Equal("LinqToGraphQL", result.Name);
+            Assert.Equal("grokys", Enumerable.Single(result.Owner).Login);
+            Assert.Equal(false, result.IsFork);
+            Assert.Equal(false, result.IsPrivate);
+        }
+    }
+}
