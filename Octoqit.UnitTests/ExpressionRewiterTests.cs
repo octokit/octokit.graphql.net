a4d7edbf4290bde415e28369eff07ae95f504333
Steven Kirk <grokys@gmail.com>
2017-02-24 08:09:46 +0100

    More tests.

---
+++ Octoqit.UnitTests/ExpressionRewiterTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using LinqToGraphQL.Builders;
+using LinqToGraphQL.Utilities;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Octoqit.UnitTests
+{
+    public class ExpressionRewiterTests
+    {
+        [Fact]
+        public void RepositoryOwner_Repository_Query()
+        {
+            var expression = new RootQuery()
+                .RepositoryOwner("foo")
+                .Repository("bar")
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                    Owner = x.Owner.Select(o => new
+                    {
+                        o.Login
+                    }),
+                    x.IsFork,
+                    x.IsPrivate,
+                });
+
+            Expression<Func<JObject, IEnumerable<object>>> expected = data =>
+                JsonUtilities.Select(
+                    data["data"]["repositoryOwner"]["repository"],
+                    x => new
+                    {
+                        Id = x["id"].ToObject<string>(),
+                        Name = x["name"].ToObject<string>(),
+                        Owner = JsonUtilities.Select(x["owner"], o => new { Login = o["login"].ToObject<string>() }),
+                        IsFork = x["isFork"].ToObject<string>(),
+                        IsPrivate = x["isPrivate"].ToObject<string>(),
+                    });
+
+            var query = new QueryBuilder().Build(expression);
+            Assert.Equal(expected.ToString(), query.Expression.ToString());
+        }
+
+        [Fact]
+        public void RepositoryOwner_Repositories_Query()
+        {
+            var expression = new RootQuery()
+                .RepositoryOwner(login: "foo")
+                .Repositories(first: 30)
+                .Edges
+                .Select(x => x.Node)
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                    Owner = x.Owner.Select(o => new
+                    {
+                        o.Login
+                    }),
+                    x.IsFork,
+                    x.IsPrivate,
+                });
+
+            Expression<Func<JObject, IEnumerable<object>>> expected = data =>
+                JsonUtilities.Select(
+                    data["data"]["repositoryOwner"]["repositories"],
+                    x => new
+                    {
+                        Id = x["id"].ToObject<string>(),
+                        Name = x["name"].ToObject<string>(),
+                        Owner = JsonUtilities.Select(x["owner"], o => new { Login = o["login"].ToObject<string>() }),
+                        IsFork = x["isFork"].ToObject<string>(),
+                        IsPrivate = x["isPrivate"].ToObject<string>(),
+                    });
+
+            var query = new QueryBuilder().Build(expression);
+            Assert.Equal(expected.ToString(), query.Expression.ToString());
+        }
+    }
+}
--- Octoqit.UnitTests/ResponseDeserializerTests.cs
+++ Octoqit.UnitTests/ResponseDeserializerTests.cs
@@ -7,89 +7,87 @@ using Xunit;
 
 namespace Octoqit.UnitTests
 {
-//    public class ResponseDeserializerTests
-//    {
-//        [Fact]
-//        public void RepositoryOwner_Repository_Query()
-//        {
-//            var query = new RootQuery()
-//                .RepositoryOwner("grokys")
-//                .Repository("VisualStudio")
-//                .Select(x => new
-//                {
-//                    x.Id,
-//                    x.Name,
-//                    Owner = x.Owner.Select(o => new
-//                    {
-//                        o.Login
-//                    }),
-//                    x.IsFork,
-//                    x.IsPrivate,
-//                });
-//            string data = @"{
-//  ""data"": {
-//    ""repositoryOwner"": {
-//      ""repository"": {
-//        ""id"": ""1234"",
-//        ""name"": ""LinqToGraphQL"",
-//        ""owner"": {
-//          ""login"": ""grokys""
-//        },
-//        ""isFork"": false,
-//        ""isPrivate"": false
-//      }
-//    }
-//  }
-//}";
+    public class ResponseDeserializerTests
+    {
+        [Fact]
+        public void RepositoryOwner_Repository_Query()
+        {
+            var expression = new RootQuery()
+                .RepositoryOwner("grokys")
+                .Repository("VisualStudio")
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                    Owner = x.Owner.Select(o => new
+                    {
+                        o.Login
+                    }),
+                    x.IsFork,
+                    x.IsPrivate,
+                });
 
-//            var operation = new QueryBuilder().Build(query.Expression);
-//            var expectedType = query.GetType().GetGenericArguments()[0];
-//            dynamic result = new ResponseDeserializer().Deserialize(operation, data);
+            string data = @"{
+  ""data"": {
+    ""repositoryOwner"": {
+      ""repository"": {
+        ""id"": ""1234"",
+        ""name"": ""LinqToGraphQL"",
+        ""owner"": {
+          ""login"": ""grokys""
+        },
+        ""isFork"": false,
+        ""isPrivate"": false
+      }
+    }
+  }
+}";
 
-//            Assert.IsType(expectedType, result);
-//            Assert.Equal("1234", result.Id);
-//            Assert.Equal("LinqToGraphQL", result.Name);
-//            Assert.Equal("grokys", Enumerable.Single(result.Owner).Login);
-//            Assert.Equal(false, result.IsFork);
-//            Assert.Equal(false, result.IsPrivate);
-//        }
+            var query = new QueryBuilder().Build(expression);
+            dynamic result = new ResponseDeserializer().Deserialize(query, data).Single();
 
-//        [Fact]
-//        public void Viewer_Email()
-//        {
-//            var query = new RootQuery().Viewer.Select(x => new { x.Email });
-//            string data = @"{
-//  ""data"": {
-//    ""viewer"": {
-//      ""email"": ""grokys@gmail.com""
-//    }
-//  }
-//}";
-//            var operation = new QueryBuilder().Build(query.Expression);
-//            var expectedType = query.GetType().GetGenericArguments()[0];
-//            dynamic result = new ResponseDeserializer().Deserialize(operation, data);
+            Assert.Equal("1234", result.Id);
+            Assert.Equal("LinqToGraphQL", result.Name);
+            Assert.Equal("grokys", Enumerable.Single(result.Owner).Login);
+            Assert.Equal(false, result.IsFork);
+            Assert.Equal(false, result.IsPrivate);
+        }
 
-//            Assert.Equal("grokys@gmail.com", result.Email);
-//        }
+        [Fact]
+        public void Viewer_Email()
+        {
+            var expression = new RootQuery().Viewer.Select(x => new { x.Email });
 
-//        [Fact]
-//        public void Viewer_Login_Email()
-//        {
-//            var query = new RootQuery().Viewer.Select(x => new { x.Login, x.Email });
-//            string data = @"{
-//  ""data"": {
-//    ""viewer"": {
-//      ""login"": ""grokys"",
-//      ""email"": ""grokys@gmail.com""
-//    }
-//  }
-//}";
-//            var operation = new QueryBuilder().Build(query.Expression);
-//            var expectedType = query.GetType().GetGenericArguments()[0];
-//            dynamic result = new ResponseDeserializer().Deserialize(operation, data);
+            string data = @"{
+  ""data"": {
+    ""viewer"": {
+      ""email"": ""grokys@gmail.com""
+    }
+  }
+}";
+            var query = new QueryBuilder().Build(expression);
+            dynamic result = new ResponseDeserializer().Deserialize(query, data).Single();
 
-//            Assert.Equal("grokys", result.Login);
-//            Assert.Equal("grokys@gmail.com", result.Email);
-//        }
-//    }
+            Assert.Equal("grokys@gmail.com", result.Email);
+        }
+
+        [Fact]
+        public void Viewer_Login_Email()
+        {
+            var expression = new RootQuery().Viewer.Select(x => new { x.Login, x.Email });
+            string data = @"{
+  ""data"": {
+    ""viewer"": {
+      ""login"": ""grokys"",
+      ""email"": ""grokys@gmail.com""
+    }
+  }
+}";
+            var query = new QueryBuilder().Build(expression);
+            dynamic result = new ResponseDeserializer().Deserialize(query, data).Single();
+
+            Assert.Equal("grokys", result.Login);
+            Assert.Equal("grokys@gmail.com", result.Email);
+        }
+    }
 }
