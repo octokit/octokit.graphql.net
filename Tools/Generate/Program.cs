daffedd53f8cd7645fce8957f291ee77122f5dd3
Steven Kirk <grokys@gmail.com>
2017-05-12 23:06:30 +0200

    Adapted sandbox to be entity generator.

---
+++ Tools/Generate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Octokit.GraphQL.Core;
+using Octokit.GraphQL.Core.Generation;
+using System.IO;
+
+namespace Generate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("GitHub GraphQL .NET entity generator");
+                Console.WriteLine("usage: Generate.exe [apitoken] [path]");
+                return;
+            }
+
+            GenerateEntities(args[0], args[1]).Wait();
+            Console.ReadKey();
+        }
+
+        private static async Task GenerateEntities(string token, string path)
+        {
+            var url = "https://api.github.com/graphql";
+            var connection = new Connection(url, token);
+
+            Console.WriteLine("Reading from " + url);
+            var schema = await SchemaReader.ReadSchema(connection);
+
+            foreach (var file in CodeGenerator.Generate(schema, "Octokit.GraphQL"))
+            {
+                Console.WriteLine("Writing " + file.FileName);
+                File.WriteAllText(Path.Combine(path, file.FileName), file.Content);
+            }
+        }
+    }
+}
