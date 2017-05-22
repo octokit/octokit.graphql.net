26b605c79413bda8e69ef2795c8a3591cb0d04e7
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-05-22 14:34:22 -0400

    Adding functionality to discover integration tests if proper environment variables are set

---
+++ Octokit.GraphQL.Core.IntegrationTests/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Octokit.GraphQL.Core.IntegrationTests
+{
+    public static class Helper
+    {
+        private static string Username => Environment.GetEnvironmentVariable("OCTOKIT_GQL_GITHUBUSERNAME");
+
+        private static string Password => Environment.GetEnvironmentVariable("OCTOKIT_GQL_GITHUBPASSWORD");
+
+        private static string OAuthToken => Environment.GetEnvironmentVariable("OCTOKIT_GQL_OAUTHTOKEN");
+
+        public static bool HasCredentials => !string.IsNullOrWhiteSpace(Username) && (!string.IsNullOrWhiteSpace(Password) || !string.IsNullOrWhiteSpace(OAuthToken));
+    }
+}
---
+++ Octokit.GraphQL.Core.IntegrationTests/IntegrationTestAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Octokit.GraphQL.Core.IntegrationTests
+{
+    public class IntegrationTestDiscoverer : IXunitTestCaseDiscoverer
+    {
+        readonly IMessageSink _diagnosticMessageSink;
+
+        public IntegrationTestDiscoverer(IMessageSink diagnosticMessageSink)
+        {
+            this._diagnosticMessageSink = diagnosticMessageSink;
+        }
+
+        public IEnumerable<IXunitTestCase> Discover(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IAttributeInfo factAttribute)
+        {
+            return !Helper.HasCredentials
+                ? Enumerable.Empty<IXunitTestCase>()
+                : new[] { new XunitTestCase(_diagnosticMessageSink, discoveryOptions.MethodDisplayOrDefault(), testMethod) };
+        }
+    }
+
+    [XunitTestCaseDiscoverer("Octokit.Tests.Integration.IntegrationTestDiscoverer", "Octokit.Tests.Integration")]
+    public class IntegrationTestAttribute : FactAttribute
+    {
+    }
+
+    public class SampleIntegrationTest
+    {
+        [IntegrationTest]
+        public void Blah()
+        {
+            
+        }
+    }
+}
