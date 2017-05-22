3f67df17cb40fdb6bb92f578d359f1c64139cfef
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-05-22 15:50:17 -0400

    Adding an integration test that compares available fields against the v3 api

--- Octokit.GraphQL.IntegrationTests/ViewerTests.cs
+++ Octokit.GraphQL.IntegrationTests/ViewerTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 using Octokit.GraphQL.Core;
 using Xunit;
 
@@ -6,14 +9,87 @@ namespace Octokit.GraphQL.IntegrationTests
 {
     public class ViewerTests
     {
+        private const string GithubComGraphqlApi = "https://api.github.com/graphql";
+
+        private static readonly Uri GithubComApiUri = new Uri("https://api.github.com/");
+
         [IntegrationTest]
         public void Viewer_By_OAuthToken_Matches_Username()
         {
-            var connection = new Core.Connection("https://api.github.com/graphql", Helper.OAuthToken);
-            var query = new Query().Viewer.Select(user => user.Login);
+            var connection = new Core.Connection(GithubComGraphqlApi, Helper.OAuthToken);
+            var query = new Query().Viewer.Select(user => new{
+                user.Login,
+                user.Email,
+                user.IsViewer
+            });
 
-            var login = connection.Run(query).Result.FirstOrDefault();
-            Assert.Equal(Helper.Username, login);
+            var graphqlUser = connection.Run(query).Result.FirstOrDefault();
+
+            Assert.NotNull(graphqlUser);
+            Assert.True(graphqlUser.IsViewer);
+
+            Assert.Equal(Helper.Username, graphqlUser.Login);
+        }
+
+        [IntegrationTest]
+        public void Viewer_By_GraphyQL_Matches_Api()
+        {
+            var credentials = new Credentials(Helper.OAuthToken);
+            var gitHubClient = new GitHubClient(new ProductHeaderValue("OctokitTests"), GithubComApiUri)
+            {
+                Credentials = credentials
+            };
+
+            var apiUser = gitHubClient.User.Current().Result;
+
+            var connection = new Core.Connection(GithubComGraphqlApi, Helper.OAuthToken);
+            var query = new Query().Viewer.Select(user => new
+            {
+                AvatarUrl = user.AvatarUrl(null),
+                user.Bio,
+                user.DatabaseId,
+                user.Login,
+                user.Email,
+                user.Company,
+                user.CreatedAt,
+                user.Name,
+                user.Url,
+                user.IsHireable,
+                user.Location,
+            });
+
+            var graphqlUser = connection.Run(query).Result.FirstOrDefault();
+
+            Assert.NotNull(graphqlUser);
+
+            Assert.Equal(apiUser.AvatarUrl, graphqlUser.AvatarUrl);
+            Assert.Equal(apiUser.Bio, graphqlUser.Bio);
+            Assert.Equal(apiUser.Company, graphqlUser.Company);
+            Assert.Equal(apiUser.CreatedAt, ParseUTCLocal(graphqlUser.CreatedAt));
+            Assert.Equal(apiUser.Email ?? string.Empty, graphqlUser.Email);
+            Assert.Equal(apiUser.Hireable ?? false, graphqlUser.IsHireable);
+            Assert.Equal(apiUser.Id, graphqlUser.DatabaseId);
+            Assert.Equal(apiUser.Location, graphqlUser.Location);
+            Assert.Equal(apiUser.Login, graphqlUser.Login);
+            Assert.Equal(apiUser.Name, graphqlUser.Name);
+            Assert.Equal(apiUser.HtmlUrl, graphqlUser.Url);
+
+            //TODO: Verify the following fields from the old api are not verifiable
+            //Followers
+            //Blog
+            //Collaborators
+            //DiskUsage
+            //Following
+            //OwnedPrivateRepos
+            //Plan
+            //PrivateGists
+            //PublicGists
+            //TotalPrivateRepos
+        }
+
+        private static DateTime ParseUTCLocal(string dateString)
+        {
+            return Convert.ToDateTime(DateTime.Parse(dateString).ToLocalTime());
         }
     }
 }
