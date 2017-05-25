cbaecfffeb502c9fabc2dab26aee800dbfdae780
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-05-25 16:43:38 -0400

    Adding a bit more to the integration test

--- Octokit.GraphQL.IntegrationTests/IssueTests.cs
+++ Octokit.GraphQL.IntegrationTests/IssueTests.cs
@@ -1,6 +1,7 @@
 
 using System.Linq;
 using Octokit.GraphQL.IntegrationTests.Utilities;
+using Xunit;
 
 namespace Octokit.GraphQL.IntegrationTests
 {
@@ -9,16 +10,36 @@ namespace Octokit.GraphQL.IntegrationTests
         [IntegrationTest]
         public void Should_Query_Issues_By_Repository()
         {
-            var query = new Query().Repository("octokit", "octokit.net").Issues(first: 3).Nodes.Select(i => i.Title);
-            var result = Enumerable.ToArray<string>(Connection.Run(query).Result);
+            var query = new Query().Repository("octokit", "octokit.net").Issues(first: 3).Nodes.Select(i => new
+            {
+                i.Title,
+                RepositoryName = i.Repository.Name,
+            });
+
+            var results = Connection.Run(query).Result.ToArray();
+            foreach (var result in results)
+            {
+                Assert.Equal("octokit.net", result.RepositoryName);
+            }
         }
 
         [IntegrationTest]
         public void Should_Query_Issues_By_State_And_Repository()
         {
-            var openState = new[] { IssueState.Open }.AsQueryable();
-            var query = new Query().Repository("octokit", "octokit.net").Issues(first: 3, states: openState).Nodes.Select(i => i.Title);
-            var result = Enumerable.ToArray<string>(Connection.Run(query).Result);
+            var openState = new[] { IssueState.Closed }.AsQueryable();
+            var query = new Query().Repository("octokit", "octokit.net").Issues(first: 3, states: openState).Nodes.Select(i => new
+            {
+                i.Title,
+                i.State,
+                RepositoryName = i.Repository.Name,
+            });
+
+            var results = Connection.Run(query).Result.ToArray();
+            foreach (var result in results)
+            {
+                Assert.Equal("octokit.net", result.RepositoryName);
+                Assert.Equal(IssueState.Closed, result.State);
+            }
         }
     }
 }
