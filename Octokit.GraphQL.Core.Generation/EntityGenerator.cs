178c19923caa9fa5e65368f80c0f82b06bfda09a
William Shepherd <iamwillshepherd@github.com>
2017-06-21 18:17:40 -0500

    Add System namespace so CLR types can be used

--- Octokit.GraphQL.Core.Generation/EntityGenerator.cs
+++ Octokit.GraphQL.Core.Generation/EntityGenerator.cs
@@ -23,6 +23,7 @@ namespace Octokit.GraphQL.Core.Generation
 
             return $@"namespace {rootNamespace}
 {{
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
@@ -49,6 +50,7 @@ namespace Octokit.GraphQL.Core.Generation
 
             return $@"namespace {rootNamespace}
 {{
+    using System;
     using System.Linq;
     using System.Linq.Expressions;
     using Octokit.GraphQL.Core;
