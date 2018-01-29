84d99502d5c2f346405c50d1720b3fa6e49099ff
Steven Kirk <grokys@gmail.com>
2018-01-29 12:33:42 +0100

    Added IQueryableInterface and Cast extension.
    Interfaces need to be cast to an actual type to be useful - this implements the case where you know what type the returned value will be.
    
    - Added `IQueryableInterface` interface
    - Added `IQueryableInterface.Cast` extension method

---
+++ Octokit.GraphQL.Core.UnitTests/Models/INode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Octokit.GraphQL.Core.UnitTests.Models
+{
+    interface INode : IQueryableInterface
+    {
+    }
+
+    internal class StubINode : QueryableValue<StubINode>, INode
+    {
+        public StubINode(Expression expression) : base(expression)
+        {
+        }
+
+        public string Id { get; }
+
+        internal static StubINode Create(Expression expression)
+        {
+            return new StubINode(expression);
+        }
+    }
+
+}
