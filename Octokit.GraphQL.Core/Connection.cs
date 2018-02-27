39b77b56b55066031e0e0d97af4d221336fd276d
Steven Kirk <grokys@gmail.com>
2018-02-27 12:47:42 +0100

    Make Connection.Run virtual.
    And refactor the sending and receiving of data into a `Send` method.

--- Octokit.GraphQL.Core/Connection.cs
+++ Octokit.GraphQL.Core/Connection.cs
@@ -15,8 +15,6 @@ namespace Octokit.GraphQL
         /// </summary>
         public static readonly Uri GithubApiUri = new Uri("https://api.github.com/graphql");
 
-        private readonly ICredentialStore credentialStore;
-        private readonly HttpClient httpClient;
 
         public Connection(ProductHeaderValue productInformation, string token)
             : this(productInformation, GithubApiUri, token)
@@ -36,25 +34,32 @@ namespace Octokit.GraphQL
         public Connection(ProductHeaderValue productInformation, Uri uri, ICredentialStore credentialStore)
         {
             Uri = uri;
-            this.credentialStore = credentialStore;
-            httpClient = CreateHttpClient(productInformation);
+            CredentialStore = credentialStore;
+            HttpClient = CreateHttpClient(productInformation);
         }
 
         public Uri Uri { get; }
+        protected ICredentialStore CredentialStore { get; }
+        protected HttpClient HttpClient { get; }
 
-        public async Task<T> Run<T>(
+        public virtual async Task<T> Run<T>(
             CompiledQuery<T> query,
             IDictionary<string, object> variables = null)
         {
             var payload = query.GetPayload(variables);
-            var token = await credentialStore.GetCredentials();
+            var data = await Send(payload);
+            var deserializer = new ResponseDeserializer();
+            return deserializer.Deserialize(query, data);
+        }
+
+        protected async Task<string> Send(string payload)
+        {
+            var token = await CredentialStore.GetCredentials();
             var request = new HttpRequestMessage(HttpMethod.Post, Uri);
             request.Content = new StringContent(payload);
             request.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
-            var response = await httpClient.SendAsync(request);
-            var data = await response.Content.ReadAsStringAsync();
-            var deserializer = new ResponseDeserializer();
-            return deserializer.Deserialize(query, data);
+            var response = await HttpClient.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
         }
 
         private HttpClient CreateHttpClient(ProductHeaderValue header)
