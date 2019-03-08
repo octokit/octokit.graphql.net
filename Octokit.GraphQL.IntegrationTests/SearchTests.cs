6eeb0f35043ab11187191bd24dc5825996143f4d
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-03-08 07:21:56 -0500

    Switch with all pages causes error

--- Octokit.GraphQL.IntegrationTests/SearchTests.cs
+++ Octokit.GraphQL.IntegrationTests/SearchTests.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Octokit.GraphQL.Core;
 using Octokit.GraphQL.Core.Builders;
 using Octokit.GraphQL.IntegrationTests.Utilities;
 using Octokit.GraphQL.Model;
+using static Octokit.GraphQL.Variable;
 
 namespace Octokit.GraphQL.IntegrationTests
 {
@@ -25,6 +28,55 @@ namespace Octokit.GraphQL.IntegrationTests
             var vars2 = new Dictionary<string, object>() { { "a", 2 }, { "b", false }, { "c", "bar" } };
 
             //var results = Connection.Run(query).Result.ToArray();
+        }
+
+        [IntegrationTest]
+        public async Task Should_Query_Issues()
+        {
+            var filter = $"type:pr repo:github/VisualStudio";
+
+            var query = new Query().Search(Var(nameof(filter)), first: 2, type: SearchType.Issue)
+                .Select(page => new 
+                {
+                    Items = page.Nodes
+                        .Select(issueOrPr => new
+                        {
+                            Result = issueOrPr.Switch<PullRequestListItem>(when =>
+                                when.PullRequest(pr => new PullRequestListItem
+                                {
+                                    Number = pr.Number,
+                                    Reviews = pr.Reviews(20, null, null, null, null, null)
+                                        .AllPages()
+                                        .Select(review => new Review
+                                        {
+                                            Body = review.Body,
+                                            CommentCount = review
+                                                .Comments(null, null, null, null)
+                                                .TotalCount,
+                                        }).ToList(),
+                                }))
+                        }).ToList()
+                }).Compile();
+
+            var vars = new Dictionary<string, object>
+            {
+                { nameof(filter), filter }
+            };
+
+            var result = await Connection.Run(query, vars);
+        }
+
+        public class PullRequestListItem
+        {
+            public int Number { get; set; }
+            public IList<Review> Reviews { get; set; }
+        }
+
+        public class Review
+        {
+            public string Body { get; set; }
+            public int CommentCount { get; set; }
+            public int Count => CommentCount + (!string.IsNullOrWhiteSpace(Body) ? 1 : 0);
         }
     }
 }
