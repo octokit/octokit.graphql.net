e2f23d834808844430aba5542000c48e0eeb05ec
Jamie Cansdale <jcansdale@github.com>
2020-07-03 15:39:28 +0100

    Check for main branch or null

--- Octokit.GraphQL.IntegrationTests/Queries/PullRequestTests.cs
+++ Octokit.GraphQL.IntegrationTests/Queries/PullRequestTests.cs
@@ -138,11 +138,22 @@ namespace Octokit.GraphQL.IntegrationTests.Queries
             var query = new Query()
                 .Repository(owner: "octokit", name: "octokit.net")
                 .PullRequest(number: 1)
-                .Select(pr => pr.BaseRef != null ? pr.BaseRef.Name : null);
+                .Select(pr => new
+                {
+                    BaseRefName = pr.BaseRef != null ? pr.BaseRef.Name : null,
+                    BaseRefNotNull = pr.BaseRef != null
+                });
 
             var result = await Connection.Run(query);
 
-            Assert.Equal("master", result);
+            if (result.BaseRefNotNull)
+            {
+                Assert.Equal("main", result.BaseRefName);
+            }
+            else
+            {
+                Assert.Null(result.BaseRefName);
+            }
         }
 
         [IntegrationTest]
@@ -159,7 +170,7 @@ namespace Octokit.GraphQL.IntegrationTests.Queries
 
             var result = await Connection.Run(query);
 
-            if(result.BaseRefNotNull)
+            if (result.BaseRefNotNull)
             {
                 Assert.Equal("octokit", result.Owner);
             }
